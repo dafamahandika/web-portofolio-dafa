284333ffdcde0eabd9b8530aea7fe5ca1b37b2f3
dafamahandika <dafamahandika@gmail.com>
2023-01-05 22:24:48 +0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,101 @@
+/*!
+ * Start Bootstrap - Freelancer v7.0.6 (https://startbootstrap.com/theme/freelancer)
+ * Copyright 2013-2022 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+ */
+//
+// Scripts
+//
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Navbar shrink function
+  var navbarShrink = function () {
+    const navbarCollapsible = document.body.querySelector("#mainNav");
+    if (!navbarCollapsible) {
+      return;
+    }
+    if (window.scrollY === 0) {
+      navbarCollapsible.classList.remove("navbar-shrink");
+    } else {
+      navbarCollapsible.classList.add("navbar-shrink");
+    }
+  };
+
+  // Shrink the navbar
+  navbarShrink();
+
+  // Shrink the navbar when page is scrolled
+  document.addEventListener("scroll", navbarShrink);
+
+  // Activate Bootstrap scrollspy on the main nav element
+  const mainNav = document.body.querySelector("#mainNav");
+  if (mainNav) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: "#mainNav",
+      offset: 72,
+    });
+  }
+
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector(".navbar-toggler");
+  const responsiveNavItems = [].slice.call(document.querySelectorAll("#navbarResponsive .nav-link"));
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener("click", () => {
+      if (window.getComputedStyle(navbarToggler).display !== "none") {
+        navbarToggler.click();
+      }
+    });
+  });
+});
+
+const txtElement = ["I'm Backend Developer"];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = "";
+let words = "";
+
+(function ngetik() {
+  if (count == txtElement.length) {
+    count = 0;
+  }
+
+  currentTxt = txtElement[count];
+
+  words = currentTxt.slice(0, ++txtIndex);
+  document.querySelector(".efek-ngetik").textContent = words;
+
+  if (words.length == currentTxt.length) {
+    count++;
+    txtIndex = 0;
+  }
+
+  setTimeout(ngetik, 150);
+})();
+
+const typed = select(".typed");
+if (typed) {
+  let typed_strings = typed.getAttribute("data-typed-items");
+  typed_strings = typed_strings.split(",");
+  new Typed(".typed", {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000,
+  });
+}
+
+function downloadFile(url, fileName) {
+  fetch(url, { method: 'get', mode: 'no-cors', referrerPolicy: 'no-referrer' })
+    .then(res => res.blob())
+    .then(res => {
+      const aElement = document.createElement('a');
+      aElement.setAttribute('download', fileName);
+      const href = URL.createObjectURL(res);
+      aElement.href = href;
+      aElement.setAttribute('target', '_blank');
+      aElement.click();
+      URL.revokeObjectURL(href);
+    });
+};
+
